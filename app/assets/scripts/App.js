cfa753f1d43cbda1281e2bf5c890369c22fc5ef1
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-02 09:41:48 +0100

    Reveal On Scroll PART I

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 // You can name MobileMenu or whatever after import syntax
 
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
